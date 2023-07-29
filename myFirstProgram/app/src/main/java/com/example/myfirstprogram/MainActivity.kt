b17c0e69616e450fe6a726c4c65f6b2382839897
Asheeshgupta10 <asheesh.kumar.gupta10@gmail.com>
2023-07-29 17:53:25 +0530

    Profile

--- myFirstProgram/app/src/main/java/com/example/myfirstprogram/MainActivity.kt
+++ myFirstProgram/app/src/main/java/com/example/myfirstprogram/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myfirstprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding  btnShowToast.setOnClickListener {
+            Log.i("this", "Button was clicked")
+            Toast.makeText(this,"Click Button call", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+
+
     }
+
 }
